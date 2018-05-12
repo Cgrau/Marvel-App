f97b774ad96532d2780c09ed7c5ce56785ae33c0
Carles Grau Galvan <carles.grau.galvan@everis.com>
2018-05-12 19:04:28 +0200

    Pass logic to API client from worker

--- Marvel-App/APIClient.swift
+++ Marvel-App/APIClient.swift
@@ -8,27 +8,77 @@
 
 import Foundation
 import Kingfisher
+import Alamofire
 
-private let worker = MainScreenWorker()
+
+fileprivate enum URLType: String {
+    case image = "?"
+    case `default` = "&"
+}
+
+private let imageSize = "/portrait_xlarge."
 
 final class APIClient {
+    
+    var baseURL = "https://gateway.marvel.com:443/v1/public/"
+    let publicKey = "4b6904ea45800a7ce5c7d09c841cd51e"
+    let privateKey = "a07365ea7e566693e58ae54f058281fe4c1f4a53"
+    
+    
+    private lazy var configURL: String = {
+        return "%@ts=%@&apikey=\(self.publicKey)&hash=%@"
+    }()
+    
+    private lazy var apiConfig: String = {
+        let ts = NSDate().timeIntervalSince1970.description
+        let md5String:String = ts+privateKey+publicKey
+        return String(format: configURL, "&", ts, md5String.md5())
+    }()
+    
+    private func getLink(url: String, urlType: URLType)->String{
+        let ts = NSDate().timeIntervalSince1970.description
+        let md5String:String = ts+privateKey+publicKey
+        return String.init(format: url + configURL, urlType.rawValue, ts, md5String.md5())
+    }
+    
+    func search(string: String, completion: @escaping (_ entity: [Character]?, _ error: Error?)->Void) {
+        let url = getLink(url: baseURL+"characters?nameStartsWith=\(string)&orderBy=name", urlType: .default)
+        request(with: url, type: ServiceResponse<Character>.self) { (result, error) in
+            return completion(result?.data?.results, nil)
+        }
+    }
+    
+    func getComicThumbnailData(url: String, completion: @escaping (_ entity: String?, _ error: Error?)->Void){
+        let defURL = getLink(url: url, urlType: .image)
+        request(with: defURL, type: ServiceResponse<Comic>.self) { (result, error) in
+            guard let data = result?.data, let thumbnail = data.results![0].thumbnail else { return }
+            let imageURL = thumbnail.path + imageSize + thumbnail.thumbnailExtension
+            return completion(imageURL, nil)
+        }
+    }
+    
+    private func request<T:Decodable>(with url: String, type: T.Type, completion: @escaping (_ result: T?, _ error: Error?) -> Void) {
+        Alamofire.request(url).responseData { (response) in
+            switch response.result {
+            case .success:
+                if let jsonData = response.result.value {
+                    do {
+                        let decoder = JSONDecoder()
+                        let product = try decoder.decode(T.self, from: jsonData)
+                        return completion(product, nil)
+                    } catch let error {
+                        print(error)
+                    }
+                }
+            case .failure(let error):
+                completion(nil, error)
+            }
+        }
+    }
+    
     static func getImageURL(downloadURL: String, extension: String) -> URL? {
-        let path = downloadURL + "/portrait_xlarge." + `extension`
+        let path = downloadURL + imageSize + `extension`
         guard let url = URL(string: path) else { return nil }
         return url
     }
-}
-
-extension Kingfisher {
-    
-//    public var kf: Kingfisher<Self> {
-//        get {
-//            return Kingfisher(self)
-//            
-//        }
-//    }
-//    
-//    guard let url = APIClient.getImageURL(downloadURL: thumbnail.path, extension: thumbnail.thumbnailExtension) else { return }
-//    let resource = ImageResource(downloadURL: url)
-//    self.thumbnail.kf.setImage(with: resource)
 }
--- Marvel-App/Scenes/MainScreen/MainScreenWorker.swift
+++ Marvel-App/Scenes/MainScreen/MainScreenWorker.swift
@@ -14,10 +14,8 @@ import CryptoSwift
 
 class MainScreenWorker
 {
-    let publicKey = "4b6904ea45800a7ce5c7d09c841cd51e"
-    let privateKey = "a07365ea7e566693e58ae54f058281fe4c1f4a53"
     
-    private let decoder = JSONDecoder()
+    private let apiClient = APIClient()
     
     // MARK: - Business Logic
     func doSomeWork()
@@ -29,59 +27,27 @@ class MainScreenWorker
         
     }
     
-    var apiConfig: String {
-        let ts = NSDate().timeIntervalSince1970.description
-        let md5String:String = ts+privateKey+publicKey
-        return "&ts=\(ts)&apikey=\(publicKey)&hash=\(md5String.md5())"
-    }
-    
+    /// Worker will get Characters starting by searchString pushed. This view is normally presented with details of first request to API.
+    ///
+    /// - Parameters:
+    ///   - view: Context which will be pushed.
+    ///   - model: Data model for the context that will be pushed.
     func search(string: String, completion: @escaping (_ entity: [Character]?, _ error: Error?)->Void) {
-        let url = "https://gateway.marvel.com:443/v1/public/characters?nameStartsWith=\(string)&orderBy=name" + apiConfig
-
-        Alamofire.request(url).responseData { (response) in
-            switch response.result {
-            case .success:
-                if let jsonData = response.result.value {
-                    do {
-                        let decoder = JSONDecoder()
-                        let product = try decoder.decode(ServiceResponse<Character>.self, from: jsonData)
-                        return completion(product.data?.results, nil)
-                    } catch let error {
-                        print(error)
-                    }
-                }
-            case .failure(let error):
+        apiClient.search(string: string) { (characters, error) in
+            if let error = error {
                 completion(nil, error)
+            } else {
+                completion(characters, nil)
             }
         }
     }
     
-    func getLink(url: String)->String{
-        let ts = NSDate().timeIntervalSince1970.description
-        var md5String:String = ts+privateKey+publicKey
-        md5String = md5String.md5()
-        return url + "?ts=\(ts)&apikey=\(publicKey)&hash=\(md5String)"
-    }
-    
     func getComicThumbnailData(url: String, completion: @escaping (_ entity: String?, _ error: Error?)->Void){
-        let defURL = getLink(url: url)
-        
-        Alamofire.request(defURL).responseData { (response) in
-            switch response.result {
-            case .success:
-                if let jsonData = response.result.value {
-                    do {
-                        let decoder = JSONDecoder()
-                        let responseModel = try decoder.decode(ServiceResponse<Comic>.self, from: jsonData)
-                        guard let data = responseModel.data, let thumbnail = data.results![0].thumbnail else { return }
-                        let imageURL = thumbnail.path + "/portrait_xlarge." + thumbnail.thumbnailExtension
-                        return completion(imageURL, nil)
-                    } catch let error {
-                        print(error)
-                    }
-                }
-            case .failure(let error):
+        apiClient.getComicThumbnailData(url: url) { (imageURL, error) in
+            if let error = error {
                 completion(nil, error)
+            } else {
+                completion(imageURL, nil)
             }
         }
     }
